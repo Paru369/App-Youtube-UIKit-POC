467605c5f474f57ed0b775816ef294d49aca2f8d
parutech <paruvitu@gmail.com>
2023-09-08 18:08:26 -0300

    create project

---
+++ AppYoutube/AppYoutube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppYoutube
+//
+//  Created by Paulo Pinheiro on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
