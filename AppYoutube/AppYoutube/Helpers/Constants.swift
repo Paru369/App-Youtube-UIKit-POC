12527d4b811a36568d6755716d2ddaa90675717a
parutech <paruvitu@gmail.com>
2023-09-11 18:20:48 -0300

    finish2

--- AppYoutube/AppYoutube/Helpers/Constants.swift
+++ AppYoutube/AppYoutube/Helpers/Constants.swift
@@ -12,12 +12,8 @@ struct Constants {
     
     
     static var API_KEY = "AIzaSyAOsNVCTe31tnWlxZ7xc-26XKHbX3egULU"
-    
-    // Tom Court PLay List
-    
-    static var TOMPLAYLIST_ID = "PLnkXOJiZ9KuP_64VYTDY1-J5P3iKXYCrH"
-          
-    static var PLAYLIST_ID = "PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
+
+    static var PLAYLIST_ID = "PLt5YPA-Y-vpo-IkkGEpGHabxfujmI8Fbf"
     
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 
