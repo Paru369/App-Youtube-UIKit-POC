f172dba243e9d7b8c5ebe219ef82d2f69c884aac
parutech <paruvitu@gmail.com>
2023-09-11 13:41:35 -0300

    feat: connect detail view

--- AppYoutube/AppYoutube/ViewController/DetailViewController.swift
+++ AppYoutube/AppYoutube/ViewController/DetailViewController.swift
@@ -17,6 +17,11 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var webView: WKWebView!
     
+    
+    
+    var video: Video?
+    
+    
     @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
@@ -25,15 +30,37 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+           
+           // Clear the fields
+           titleLabel.text = ""
+           dateLabel.text = ""
+           textView.text = ""
+           
+           // Check if there's a video
+           guard video != nil else {
+               return
+           }
+           
+           // Create the embed URL
+           let embedUrlString = Constants.YT_EMBED_URL + video!.videoId
+           
+           // Load it into the webview
+           let url = URL(string: embedUrlString)
+           let request = URLRequest(url: url!)
+           webView.load(request)
+           
+           // Set the title
+           titleLabel.text = video!.title
+           
+           // Set the date
+           let df = DateFormatter()
+           df.dateFormat = "EEEE, MMM d, yyyy"
+           dateLabel.text = df.string(from: video!.published)
+           
+           // Set the description
+           textView.text = video!.description
     }
-    */
 
 }
